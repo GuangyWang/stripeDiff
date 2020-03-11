9ee6523370193fe08e7b45e2504f1a091d808cbd
szhang32 <shuozhang23@gmail.com>
2020-03-11 09:49:19 -0500

    Create StripeDiff.sh

---
+++ src/StripeDiff.sh
@@ -0,0 +1,7 @@
+
+
+### combine called differential stripes
+# p_cutoff: output only differential stripes whose P-value is <= p_cutoff
+# indir: a directory that contains called differential stripes
+# outdir: the directory that stores combined stripes
+python /home/tmhsxz37/archive/stripe_finder/CH12/scripts/combine_stripes.py $p_cutoff $indir $outdir

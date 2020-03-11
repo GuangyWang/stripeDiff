01465fb84f5add748b970b57fa43e230da94bac5
szhang32 <shuozhang23@gmail.com>
2020-03-11 09:50:55 -0500

    Update StripeDiff.sh

--- src/StripeDiff.sh
+++ src/StripeDiff.sh
@@ -4,4 +4,4 @@
 # p_cutoff: output only differential stripes whose P-value is <= p_cutoff
 # indir: a directory that contains called differential stripes
 # outdir: the directory that stores combined stripes
-python /home/tmhsxz37/archive/stripe_finder/CH12/scripts/combine_stripes.py $p_cutoff $indir $outdir
+python ${StripeDiff_dir}/src/combine_stripes.py $p_cutoff $indir $outdir
